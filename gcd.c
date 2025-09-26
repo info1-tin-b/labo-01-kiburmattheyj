1717e120ba5763bf542d0081c99104bd3869c8b7
kibur Lathrop matthey <emmet.brown@heig-vd.ch>
2025-09-26 20:42:15 +0200

    Fix bugs + add comments

--- gcd.c
+++ gcd.c
@@ -4,30 +4,31 @@
  * The Euclidean algorithm (also called Euclid's algorithm) is an
  * algorithm to determine the greatest common divisor of two integers.
  */
-#include <stdlib.h>
+#include <stdlib.h>// Inclus la bibliothèque stdlib pour pouvoir utiliser la fonction `atoi`
 #include <string.h>
-
-int gcd(int a, int b) {
-  int k;
-  while (b != 0) {
-    r = b;
-    b = b % a;
-    a = r;
+#include <stdio.h>
+// Fonction qui calcule le plus grand commun diviseur de deux entiers a et b
+int gcd(int a, int b) {     // Déclare la fonction gcd qui prend en paramètre 2 entiers a et b et retourne un entier
+  int r;      // Déclare une variable `r` qui est le reste de la division
+  while (b != 0) {      // Tant que b n’est pas zéro
+    r = b;      // Sauvegarde la valeur de b dans r
+    b = b % a;    // Calcule le reste de a divisé par b et le stocke dans b
+    a = r;      // Affecte l’ancienne valeur de b (r) à a
   }
-  return a;
+return a;    // Quand b vaut 0, a contient le pgcd, on le retourne
 }
-
-int main(int argc, char* argv[]) {
-    if (argc <= 2) {
-        return -1;
+// Fonction principale
+int main(int argc, char* argv[]) {    
+    if (argc <= 2) {      // Vérifie qu’on a bien fourni deux arguments
+        return -1;      // Sinon, on quitte le programme avec un code d’erreur
     }
 
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
+    int a = atoi(argv[1]);    // Convertit le premier argument en entier
+    int b = atoi(argv[2]);    // Convertit le deuxième argument en entier
 
-    int result = gcd(a, b);
+    int result = gcd(a, b);   // Appelle la fonction gcd pour calculer le pgcd
 
-    printf("%d\n", result + 5);
+    printf("%d\n", result);   // Affiche le résultat dans le terminal
 
-    return 0
+    return 0;   // Retourne 0 pour indiquer que le programme s’est terminé correctement
 }
